4b7c7a291a5a47abb81cdbc40e38f3b311d5bc38
lowo1reng <38468503+lowo1reng@users.noreply.github.com>
2020-01-15 18:53:27 +0700

    Update mgid.js

--- mgid.js
+++ mgid.js
@@ -1,1 +1,1 @@
-(function () { var script = document.createElement('script'); script.src = "//jsc.mgid.com/d/i/dibuka.site.753274.js?t=" + ((d = new Date()) ? '' + d.getUTCFullYear() + d.getUTCMonth() + d.getUTCDate() + d.getUTCHours() : ''); script.async = true; document.body.appendChild(script); })();
+(function () { var script = document.createElement('script'); script.src = "//jsc.mgid.com/d/i/dibuka.site.786218.js?t=" + ((d = new Date()) ? '' + d.getUTCFullYear() + d.getUTCMonth() + d.getUTCDate() + d.getUTCHours() : ''); script.async = true; document.body.appendChild(script); })();
