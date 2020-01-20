815fe4babba471df20ba75a97229330dc6d1f553
lowo1reng <38468503+lowo1reng@users.noreply.github.com>
2020-01-20 14:49:01 +0700

    Delete mgid.js

--- mgid.js
+++
@@ -1,1 +0,0 @@
-(function () { var script = document.createElement('script'); script.src = "//jsc.mgid.com/d/i/dibuka.site.786218.js?t=" + ((d = new Date()) ? '' + d.getUTCFullYear() + d.getUTCMonth() + d.getUTCDate() + d.getUTCHours() : ''); script.async = true; document.body.appendChild(script); })();
